3485043ef7483b751cc89ffb94bba3673cf2bcb7
CommanderGL <crossscar2@outlook.com>
2023-07-04 13:39:55 -0700

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://commandergl.github.io/',
+    base: '/portfolio/'
+});
