5a5a8e11c789577f0c315b15002e916b7403aed6
CommanderGL <crossscar2@outlook.com>
2023-07-11 22:20:30 -0700

    Add Birthday Website project page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
+import houstonTheme from './houston.json';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://commandergl.github.io/',
-    base: '/portfolio/'
+  site: 'https://commandergl.github.io/',
+  base: '/portfolio/',
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: houstonTheme
+    }
+  },
+  integrations: [mdx()]
 });
---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+import cssnanoPlugin from 'cssnano'
+import postcssPresetEnvPlugin from 'postcss-preset-env';
+
+export default {
+    plugins: [
+        cssnanoPlugin,
+        postcssPresetEnvPlugin
+    ],
+}
